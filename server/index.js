6ffb480e947ec11be1fcf45ab5b20bf0e4d0e307
jed <jolix123@gmail.com>
2024-02-29 17:43:53 +0100

    refactoring;

--- server/index.js
+++ server/index.js
@@ -23,13 +23,12 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-const server = app.listen(process.env.PORT, () =>
+const server= app.listen(process.env.PORT, () =>
     console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
     cors: {
-        origin: "https://let-s-talk-backend-delta.vercel.app",
+        origin: "*",
         credentials: true,
     },
 });
